8bf48364eed4c43e846472f230128247d577dab7
Mike Gestring <mgestring@hotmail.com>
2013-12-13 17:47:59 -0700

    Additional Improvements to Site

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
   get "therorlist" => "pages#therorlist"
+  get "reportahire" => "pages#reportahire"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
